d68ec2c9b1b136434b9a8487f9cbe8b3985e7e89
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2016-03-30 11:50:58 -0700

    Making version of the fusion.exe derive from the build date

--- Source/Fusion/AssemblyInfo.cs
+++ Source/Fusion/AssemblyInfo.cs
@@ -26,16 +26,3 @@ using System.Resources;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("34f0092c-133c-4891-81ed-8bfbdce29fd8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/Fusion/VersionInfo.cs
@@ -0,0 +1,3 @@
+// Do not change this file manually as it will be regenerated during the next build. To change version edit project file instead.
+[assembly: System.Reflection.AssemblyVersion("1.16.03.30")]
+[assembly: System.Reflection.AssemblyFileVersion("1.16.03.30")]
