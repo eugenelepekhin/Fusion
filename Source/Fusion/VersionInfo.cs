d0897fd5ed9fc1a06053043c6912795dbb0eba21
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2017-10-01 15:30:16 -0700

    Updating version

--- Source/Fusion/VersionInfo.cs
+++ Source/Fusion/VersionInfo.cs
@@ -1,3 +1,3 @@
 // Do not change this file manually as it will be regenerated during the next build. To change version edit project file instead.
-[assembly: System.Reflection.AssemblyVersion("1.16.03.30")]
-[assembly: System.Reflection.AssemblyFileVersion("1.16.03.30")]
+[assembly: System.Reflection.AssemblyVersion("1.17.10.01")]
+[assembly: System.Reflection.AssemblyFileVersion("1.17.10.01")]
