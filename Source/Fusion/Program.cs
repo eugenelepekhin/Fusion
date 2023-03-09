fe6e515069d15d05a6a07f7a8af9d5aa16b8860c
Eugene Lepekhin <eugene.lepekhin@gmail.com>
2023-03-08 22:51:45 -0800

    Adding more command line arguments and packaging grammar files instead of BNF.

--- Source/Fusion/Program.cs
+++ Source/Fusion/Program.cs
@@ -1,43 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 using System.Threading;
+using CommandLineParser;
 
 namespace Fusion {
-	class Program {
+	internal class Program {
+		private struct Arguments {
+			private string? inputFile;
+			private string? outputFile;
+			private List<string>? searchPath;
+
+			public bool Parse(string[] args) {
+				string? path = null;
+				List<string> arguments = new List<string>();
+				bool showHelp = false;
+				CommandLine commandLine = new CommandLine()
+					.AddString("include", "i", "<path>", "Semicolon separated list of folders to search for includes", false, p=> path = p)
+					.AddFlag("help", "?", "Show this help", false, h => showHelp = h)
+				;
+				string? errors = commandLine.Parse(args, a => arguments.AddRange(a));
+				if(showHelp || !string.IsNullOrEmpty(errors) || arguments.Count != 2) {
+					Program.Usage(errors, commandLine.Help());
+					return false;
+				}
+				if(!File.Exists(arguments[0])) {
+					Console.Error.WriteLine(Resource.FileNotFound(arguments[0]));
+					Program.Usage(null, commandLine.Help());
+					return false;
+				}
+				string? outDir = Path.GetDirectoryName(arguments[1]);
+				if(!string.IsNullOrEmpty(outDir) && !Directory.Exists(outDir)) {
+					Console.Error.WriteLine(Resource.FileNotFound(arguments[1]));
+					Program.Usage(null, commandLine.Help());
+					return false;
+				}
+				List<string> list = new List<string>();
+				if(!string.IsNullOrEmpty(path)) {
+					list.AddRange(path.Split(';').Where(d => !string.IsNullOrWhiteSpace(d)));
+					bool notFound = false;
+					foreach(string p in list) {
+						if(!Directory.Exists(p)) {
+							Console.Error.WriteLine(Resource.IncludeFolderNotFound(p));
+							notFound = true;
+						}
+					}
+					if(notFound) {
+						return false;
+					}
+				}
+				this.inputFile = arguments[0];
+				this.outputFile = arguments[1];
+				this.searchPath = list;
+				return true;
+			}
+
+			public string InputFile() {
+				Debug.Assert(this.inputFile != null);
+				return this.inputFile;
+			}
+
+			public string OutputFile() {
+				Debug.Assert(this.outputFile != null);
+				return this.outputFile;
+			}
+
+			public IEnumerable<string> SearchPath() {
+				Debug.Assert(this.searchPath != null);
+				return this.searchPath;
+			}
+		}
+
 		[SuppressMessage("Microsoft.Design", "CA1031:Do not catch general exception types")]
 		internal static int Main(string[] args) {
 			Console.Out.WriteLine(Resource.AppTitle(typeof(Program).Assembly.GetName().Version!.ToString(3)));
 			int returnCode = 1;
 			try {
-				if(args == null || args.Length != 2) {
-					Console.Error.WriteLine(Resource.Usage);
-				} else if(!File.Exists(args[0])) {
-					Console.Error.WriteLine(Resource.FileNotFound(args[0]));
-					Console.Error.WriteLine(Resource.Usage);
-				} else {
+				Arguments arguments = new Arguments();
+				if(arguments.Parse(args)) {
 					using(MemoryStream bin = new MemoryStream(16 * 1024)) {
 						using(BinaryWriter writer = new BinaryWriter(bin)) {
-							Assembler assembler = new Assembler(Console.Error, Console.Out, writer, new List<string>());
-							Exception? exception = Program.RunOnBigStack(() => assembler.Compile(args[0]));
+							Assembler assembler = new Assembler(Console.Error, Console.Out, writer, arguments.SearchPath());
+							Exception? exception = Program.RunOnBigStack(() => assembler.Compile(arguments.InputFile()));
 							if(exception != null) {
 								if(exception is FileNotFoundException fileNotFoundException) {
-									//assembler.ErrorOutput.WriteLine(fileNotFoundException.Message);
+									assembler.ErrorOutput.WriteLine(fileNotFoundException.Message);
 								} else {
 									assembler.ErrorOutput.WriteLine(exception.ToString());
 								}
-								returnCode = 1;
 							} else {
 								if(assembler.ErrorCount <= 0) {
-									returnCode = 0;
 									writer.Flush();
-									using(FileStream file = File.Create(args[1])) {
+									using(FileStream file = File.Create(arguments.OutputFile())) {
 										bin.WriteTo(file);
 									}
 								}
 								if(assembler.ErrorCount == 0) {
 									assembler.StandardOutput.WriteLine(Resource.SummarySuccess);
+									returnCode = 0;
 								} else {
 									assembler.StandardOutput.WriteLine(Resource.SummaryErrors(assembler.ErrorCount));
 								}
@@ -47,10 +110,8 @@ namespace Fusion {
 				}
 			} catch(FileNotFoundException fileNotFoundException) {
 				Console.Error.WriteLine(fileNotFoundException.Message);
-				returnCode = 1;
 			} catch(Exception exception) {
 				Console.Error.WriteLine(exception.ToString());
-				returnCode = 1;
 			}
 			return returnCode;
 		}
@@ -70,6 +131,14 @@ namespace Fusion {
 			thread.Start();
 			thread.Join();
 			return exception;
+		}
+
+		private static void Usage(string? errors, string argHelp) {
+			if(!string.IsNullOrEmpty(errors)) {
+				Console.Error.WriteLine(errors);
+			}
+			Console.Out.WriteLine(Resource.Usage);
+			Console.Out.WriteLine(argHelp);
 		}
 	}
 }
--- Source/fusion.cmd
+++ Source/fusion.cmd
@@ -1,3 +1,3 @@
 @echo off
 set location=%~dp0
-dotnet "%location%Fusion.dll" "%~f1" "%~f2"
+dotnet "%location%Fusion.dll" %*
